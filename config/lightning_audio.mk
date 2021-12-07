894303f76126f2a20de4e26d26efaefd31bd3502
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-12-07 15:54:26 -0500

    Update Lock & Unlock sound

--- config/lightning_audio.mk
+++ config/lightning_audio.mk
@@ -14,9 +14,9 @@ PRODUCT_COPY_FILES += \
     $(RINGTONE_PATH)/Highscore.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Highscore.ogg \
     $(NOTIFICATION_PATH)/PowerOn.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/PowerOn.ogg \
     $(NOTIFICATION_PATH)/PowerOff.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/PowerOff.ogg \
+    $(NOTIFICATION_PATH)/Pinball.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Pinball.ogg \
     $(NOTIFICATION_PATH)/Untitled.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Untitled.ogg \
     $(NOTIFICATION_PATH)/Beautiful_Chimes.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Beautiful_Chimes.ogg
-
 
 # Notifications
 PRODUCT_COPY_FILES += \
@@ -35,8 +35,9 @@ PRODUCT_COPY_FILES += \
     $(RINGTONE_PATH)/Happy.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Happy.ogg \
     $(RINGTONE_PATH)/Smooth.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Smooth.ogg
 
-
 # UI
 PRODUCT_COPY_FILES += \
+    $(UI_PATH)/Lock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Lock.ogg \
+    $(UI_PATH)/Unlock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Unlock.ogg \
     $(UI_PATH)/ChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/ChargingStarted.ogg \
     $(UI_PATH)/WirelessChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/WirelessChargingStarted.ogg
